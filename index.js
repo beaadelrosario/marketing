acd91751fd8c18ce6120c4908926a50f44057eab
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-06-25 16:45:41 -0700

    animation pt 3

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ gsap.from(".whoBox", {duration:1.5, opacity:0, ease:"back", y:150, stagger:.5});
 
 //gsap.from - where its coming from - ease:how it lands, stagger gives a delay
 
-gsap.from(".step", {delay:1, opacity:0, ease:"back", y:150, stagger:1});
+gsap.from(".step", {duration:2, opacity:0, ease:"back", y:-150, stagger:1});
